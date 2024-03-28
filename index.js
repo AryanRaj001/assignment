57f7700f484dda1d1738852e1968d7a4a0dee284
Aryan Raj <109361430+AryanRaj001@users.noreply.github.com>
2024-03-28 09:48:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/db");
+const Product = require("./db");
 
 const app = express();
 const PORT = 3000;
